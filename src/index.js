1a681b63f61964f52d64f565689342c829e339d5
Cao <cw3180@163.com>
2019-11-29 01:56:49 +0800

    test husky command

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import EnhanceMultipleCascader from "./cascader.vue";
+import EnhanceMultipleCascader from './cascader.vue'
 
-function install(_Vue) {
-  _Vue.component("EnhanceMultipleCascader", EnhanceMultipleCascader);
+function install (_Vue) {
+  _Vue.component('EnhanceMultipleCascader', EnhanceMultipleCascader)
 }
 
-if (typeof window !== "undefined" && window.Vue) {
-  install(window.Vue);
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue)
 }
 
 const enhanceMultipleCascader = {
   EnhanceMultipleCascader
-};
+}
 
-export default Object.assign(enhanceMultipleCascader, { install });
+export default Object.assign(enhanceMultipleCascader, { install })
