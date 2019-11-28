2f01152c243ab69b4a6168ee0dac4d09c95a6751
Cao <cw3180@163.com>
2019-11-28 20:45:39 +0800

    add:standard/standard module to uniform code style.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import EnhanceMultiCascader from "./cascader.vue";
+import EnhanceMultiCascader from './cascader.vue'
 
-function install(_Vue) {
-  _Vue.component("EnhanceMultiCascader", EnhanceMultiCascader);
+function install (_Vue) {
+  _Vue.component('EnhanceMultiCascader', EnhanceMultiCascader)
 }
 
-if (typeof window !== "undefined" && window.Vue) {
-  install(window.Vue);
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue)
 }
 
 const enhanceMultiCascader = {
   EnhanceMultiCascader
-};
-export default Object.assign(enhanceMultiCascader, { install });
+}
+export default Object.assign(enhanceMultiCascader, { install })
